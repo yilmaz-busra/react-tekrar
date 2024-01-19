fea06316db35769e5646f3aff5bbb96aaa6e11e5
busra <b.busraayilmaz@gmail.com>
2024-01-19 19:13:37 +0300

    DeleteClick-function-created&onDelete-sent

--- Task/jiraaplication/src/Components/TaskShow.js
+++ Task/jiraaplication/src/Components/TaskShow.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-function TaskShow({ task }) {
+function TaskShow({ task, onDelete }) {
   console.log(task);
+
+  const handleDeleteClick = () => {
+    onDelete(task.id);
+  };
   return (
     <div className="task-show">
       <h3 className="task-title">Göreviniz</h3>
@@ -9,7 +13,9 @@ function TaskShow({ task }) {
       <h3 className="task-title">Yapılacaklar</h3>
       <p>{task.taskDesc}</p>
       <div>
-        <button className="task-delete">Sil</button>
+        <button className="task-delete" onClick={handleDeleteClick}>
+          Sil
+        </button>
         <button className="task-edit">Güncelle</button>
       </div>
     </div>

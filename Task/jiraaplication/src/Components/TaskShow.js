6c67f8493c03bf5a821a177bb2f695b96009a339
busra <b.busraayilmaz@gmail.com>
2024-01-19 18:32:57 +0300

    listing-added

--- Task/jiraaplication/src/Components/TaskShow.js
+++ Task/jiraaplication/src/Components/TaskShow.js
@@ -2,7 +2,18 @@ import React from "react";
 
 function TaskShow({ task }) {
   console.log(task);
-  return <div></div>;
+  return (
+    <div className="task-show">
+      <h3 className="task-title">Göreviniz</h3>
+      <p>{task.title}</p>
+      <h3 className="task-title">Yapılacaklar</h3>
+      <p>{task.taskDesc}</p>
+      <div>
+        <button className="task-delete">Sil</button>
+        <button className="task-edit">Güncelle</button>
+      </div>
+    </div>
+  );
 }
 
 export default TaskShow;

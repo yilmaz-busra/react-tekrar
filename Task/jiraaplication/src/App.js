48ec4221d6bd678d12b4926e9c6649a0360a3c38
busra <b.busraayilmaz@gmail.com>
2024-01-19 17:30:26 +0300

    values-in-taskcreate-added-to-app-as-prop

--- Task/jiraaplication/src/App.js
+++ Task/jiraaplication/src/App.js
@@ -2,9 +2,12 @@ import "./App.css";
 import TaskCreate from "./Components/TaskCreate";
 
 function App() {
+  const createTask = (title, taskDesc) => {
+    console.log(title, taskDesc);
+  };
   return (
     <div className="App">
-      <TaskCreate />
+      <TaskCreate onCreate={createTask} />
       <h1>Görevler</h1>
     </div>
   );
--- Task/jiraaplication/src/Components/TaskCreate.js
+++ Task/jiraaplication/src/Components/TaskCreate.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
-function TaskCreate() {
+function TaskCreate({ onCreate }) {
+  const [title, setTitle] = useState();
+  const [taskDesc, setTaskDesc] = useState();
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleTaskChange = (event) => {
+    setTaskDesc(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onCreate(title, taskDesc);
+    setTitle("");
+    setTaskDesc("");
+  };
   return (
     <div className="task-create">
       <h3>Lütfen Task Ekleyin!</h3>
       <form className="task-form">
         <label className="task-label"> Başlık</label>
-        <input />
-        <label>Task Girin!</label>
-        <textarea />
-        <button>Oluştur</button>
+        <input value={title} onChange={handleChange} className="task-input" />
+        <label className="task-label">Task Girin!</label>
+        <textarea
+          value={taskDesc}
+          onChange={handleTaskChange}
+          className="task-input"
+          rows={5}
+        />
+        {/* rows={5} ==> textareanın satir sayisini, yüksekligini ayarlar  */}
+        <button className="task-button" onClick={handleSubmit}>
+          Oluştur
+        </button>
       </form>
     </div>
   );

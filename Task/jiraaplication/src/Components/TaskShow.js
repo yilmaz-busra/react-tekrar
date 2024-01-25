190a3ac21521c050dc87c9e3d7e56997b79d4f06
busra <b.busraayilmaz@gmail.com>
2024-01-25 19:22:32 +0300

    taskeditShow-add

--- Task/jiraaplication/src/Components/TaskShow.js
+++ Task/jiraaplication/src/Components/TaskShow.js
@@ -1,23 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import TaskCreate from "./TaskCreate";
 
 function TaskShow({ task, onDelete }) {
   console.log(task);
+  const [showEdit, setShowEdit] = useState();
 
   const handleDeleteClick = () => {
     onDelete(task.id);
   };
+  const handleEditClick = () => {
+    setShowEdit(true);
+  };
   return (
     <div className="task-show">
-      <h3 className="task-title">Göreviniz</h3>
-      <p>{task.title}</p>
-      <h3 className="task-title">Yapılacaklar</h3>
-      <p>{task.taskDesc}</p>
-      <div>
-        <button className="task-delete" onClick={handleDeleteClick}>
-          Sil
-        </button>
-        <button className="task-edit">Güncelle</button>
-      </div>
+      {showEdit ? (
+        <TaskCreate task={task} taskformUpdate={true} />
+      ) : (
+        <div>
+          <h3 className="task-title">Göreviniz</h3>
+          <p>{task.title}</p>
+          <h3 className="task-title">Yapılacaklar</h3>
+          <p>{task.taskDesc}</p>
+          <div>
+            <button className="task-delete" onClick={handleDeleteClick}>
+              Sil
+            </button>
+            <button className="task-edit" onClick={handleEditClick}>
+              Güncelle
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }

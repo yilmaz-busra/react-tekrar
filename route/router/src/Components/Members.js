4d5c7b6661f3459889b250b74d96bcfea1fbfab8
busra <b.busraayilmaz@gmail.com>
2024-02-02 02:26:21 +0300

    useParamsAdded

--- route/router/src/Components/Members.js
+++ route/router/src/Components/Members.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
 
 function Members() {
-  return <div>Members</div>;
+  // useSearchParams vasıtasıyla filtreleme islemi yapılır
+  const [searchParams, setSearchParams] = useSearchParams();
+  const isActive = searchParams.get("filter") === "active";
+  return (
+    <>
+      <div>Members</div>
+      <button onClick={() => setSearchParams({ filter: "active" })}>
+        Aktif Üyeler
+      </button>
+      <button onClick={() => setSearchParams()}>Tüm Üyeler</button>
+      {isActive ? <h2>Aktif Üyeler</h2> : <h2>Tüm Üyeler</h2>}
+    </>
+  );
 }
 
 export default Members;

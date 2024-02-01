c6e55e05662124e4f2f131db855a072530bbbb29
busra <b.busraayilmaz@gmail.com>
2024-02-02 01:39:27 +0300

    button-added&directed-to-Misson-v-useNavigate

--- route/router/src/Components/Home.js
+++ route/router/src/Components/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <div>Home</div>
+      <button onClick={() => navigate("/mission")}>Mission a Git</button>
+    </div>
+  );
 }
 
 export default Home;

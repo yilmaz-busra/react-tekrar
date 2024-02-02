7feaf0caf3ef572f2c40b045b106540ab59dde4c
busra <b.busraayilmaz@gmail.com>
2024-02-03 00:35:49 +0300

    Data-Created

---
+++ burger-project/src/helpers/Data.js
@@ -0,0 +1,76 @@
+import BurgerImage from "../assets/burger.jpg";
+
+export const Data = [
+  {
+    name: "Efso Burger",
+    image: BurgerImage,
+    content: "150gr kasap Burger, Patates, Turşu",
+    price: 200,
+  },
+  {
+    name: "Efso Burger",
+    image: BurgerImage,
+    content: "150gr kasap Burger, Patates, Turşu",
+    price: 200,
+  },
+  {
+    name: "Efso Burger",
+    image: BurgerImage,
+    content: "150gr kasap Burger, Patates, Turşu",
+    price: 200,
+  },
+  {
+    name: "Efso Burger",
+    image: BurgerImage,
+    content: "150gr kasap Burger, Patates, Turşu",
+    price: 200,
+  },
+  {
+    name: "Efso Burger",
+    image: BurgerImage,
+    content: "150gr kasap Burger, Patates, Turşu",
+    price: 200,
+  },
+  {
+    name: "Efso Burger",
+    image: BurgerImage,
+    content: "150gr kasap Burger, Patates, Turşu",
+    price: 200,
+  },
+  {
+    name: "Efso Burger",
+    image: BurgerImage,
+    content: "150gr kasap Burger, Patates, Turşu",
+    price: 200,
+  },
+  {
+    name: "Efso Burger",
+    image: BurgerImage,
+    content: "150gr kasap Burger, Patates, Turşu",
+    price: 200,
+  },
+  {
+    name: "Efso Burger",
+    image: BurgerImage,
+    content: "150gr kasap Burger, Patates, Turşu",
+    price: 200,
+  },
+  {
+    name: "Efso Burger",
+    image: BurgerImage,
+    content: "150gr kasap Burger, Patates, Turşu",
+    price: 200,
+  },
+  {
+    name: "Efso Burger",
+    image: BurgerImage,
+    content: "150gr kasap Burger, Patates, Turşu",
+    price: 200,
+  },
+  {
+    name: "Efso Burger",
+    image: BurgerImage,
+    content: "150gr kasap Burger, Patates, Turşu",
+    price: 200,
+  },
+];

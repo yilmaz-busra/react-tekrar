b47dc422a187e1faf26ebbc636642d2878fc79d2
busra <b.busraayilmaz@gmail.com>
2024-02-02 21:22:05 +0300

    first-commit

---
+++ burger-project/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "../../burger-project/src/Components/Home";
+
+import Contact from "../../burger-project/src/Components/Contact";
+import Menu from "./Components/Menu";
+import About from "./Components/About";
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;

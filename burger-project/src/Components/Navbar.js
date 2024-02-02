bb10d710ae6a9263d7b99577830e178922d8090d
busra <b.busraayilmaz@gmail.com>
2024-02-03 00:36:34 +0300

    NavbarCreated&&Redirected-via-link

--- burger-project/src/Components/Navbar.js
+++ burger-project/src/Components/Navbar.js
@@ -1,14 +1,14 @@
 import React from "react";
 import BurgerLogo from "../assets/burgerlogo.png";
 import { Link } from "react-router-dom";
-
+import "../styles/Navbar.css";
 function Navbar() {
   return (
     <div className="navbar">
       <div className="main">
         <img src={BurgerLogo} alt="" />
       </div>
-      <div>
+      <div className="mainLink">
         <Link to={"/"}>Anasayfa</Link>
         <Link to={"/menu"}>Menu</Link>
         <Link to={"/about"}>Hakkımızda</Link>

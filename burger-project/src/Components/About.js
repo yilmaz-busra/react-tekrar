605583841e52435f6fd32659d4495cfac3f8b4ba
busra <b.busraayilmaz@gmail.com>
2024-02-03 02:24:33 +0300

    AboutCreated&Styled

--- burger-project/src/Components/About.js
+++ burger-project/src/Components/About.js
@@ -1,7 +1,30 @@
 import React from "react";
+import banner from "../assets/banner-yeni.jpeg";
+import "../styles/About.css"; // Make sure to import the CSS file
 
-function About() {
-  return <div>About</div>;
-}
+export const About = () => {
+  return (
+    <div className="about">
+      <div
+        className="aboutTop"
+        style={{ backgroundImage: `url(${banner})` }}
+      ></div>
 
+      <div className="aboutBottom">
+        <h1>Hakkımızda</h1>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </p>
+      </div>
+    </div>
+  );
+};
 export default About;

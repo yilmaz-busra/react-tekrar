7753095cbe07df982c2cd27a88295b731f73dd67
busra <b.busraayilmaz@gmail.com>
2024-02-03 01:51:19 +0300

    Data-mapped

--- burger-project/src/Components/Menu.js
+++ burger-project/src/Components/Menu.js
@@ -1,21 +1,27 @@
 import React from "react";
 import { Data } from "../helpers/Data";
 import MenuItem from "./MenuItem";
+import "../styles/Menu.css";
 
-export const Menu = () => {
+const Menu = () => {
   return (
     <div className="menu">
       <h1 className="menuTitle">Burgerlerimiz</h1>
       <div className="menuList">
         {Data.map((menuItem, key) => {
-          <MenuItem
-            image={menuItem.image}
-            name={menuItem.name}
-            content={menuItem.content}
-            price={menuItem.price}
-          />;
+          return (
+            <MenuItem
+              key={key}
+              image={menuItem.image}
+              name={menuItem.name}
+              content={menuItem.content}
+              price={menuItem.price}
+            />
+          );
         })}
       </div>
     </div>
   );
 };
+
+export default Menu;

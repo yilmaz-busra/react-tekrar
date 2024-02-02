a7340632d0e90642e1371bbfe0605d8e1d27407e
busra <b.busraayilmaz@gmail.com>
2024-02-02 21:39:17 +0300

    Navbar-imported

--- burger-project/src/App.js
+++ burger-project/src/App.js
@@ -5,9 +5,12 @@ import Home from "../../burger-project/src/Components/Home";
 import Contact from "../../burger-project/src/Components/Contact";
 import Menu from "./Components/Menu";
 import About from "./Components/About";
+import Navbar from "./Components/Navbar";
+
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />

69713efb6b052d73536b45c34292bb7ebc5d3450
busra <b.busraayilmaz@gmail.com>
2024-02-03 00:34:59 +0300

    Bg-given&&LinkAdded

--- burger-project/src/Components/Home.js
+++ burger-project/src/Components/Home.js
@@ -1,7 +1,17 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import banner from "../assets/banner-yeni.jpeg";
+import "../styles/Home.css";
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="mainPage" style={{ backgroundImage: `url(${banner})` }}>
+      <div className="order">
+        <Link to={"/menu"}>
+          <button>Sipariş Ver</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;

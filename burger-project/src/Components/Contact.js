60f07758d593add5b9f6e4577c0b5555c71805d9
busra <b.busraayilmaz@gmail.com>
2024-02-03 18:47:11 +0300

    ContactAdded

--- burger-project/src/Components/Contact.js
+++ burger-project/src/Components/Contact.js
@@ -1,7 +1,37 @@
 import React from "react";
-
-function Contact() {
-  return <div>Contact</div>;
-}
-
+import burger from "../assets/burgerM.jpg";
+import "../styles/Contact.css";
+export const Contact = () => {
+  return (
+    <div className="contact">
+      <div
+        className="leftSide"
+        style={{ backgroundImage: `url(${burger})` }}
+      ></div>
+      <div className="rightSide">
+        <h1>Bizimle İletişime Geçin</h1>
+        <form>
+          <label>Ad Soyad</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Lütfen adınızı soyadınızı girin.."
+          />
+          <label>E mail</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Lütfen mailinizi girin.."
+          />
+          <label>Mesajınızı Girin</label>
+          <textarea
+            rows="6"
+            name="message"
+            placeholder="Lütfen mesajınızı girin.."
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
 export default Contact;

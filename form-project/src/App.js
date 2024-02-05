7d290fca17b1bfb150bf5f7756effc0d32809b02
busra <b.busraayilmaz@gmail.com>
2024-02-05 21:09:08 +0300

    GeneralForm&PortalForm-imorted&&Redirection-done

--- form-project/src/App.js
+++ form-project/src/App.js
@@ -1,7 +1,17 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import GeneralForm from "./Components/GeneralForm";
+import PortalForm from "./Components/PortalForm";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<GeneralForm />} />
+        <Route path="/portal" element={<PortalForm />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;

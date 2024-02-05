9d59b0c2fd5385cf0aec5bb5367fed5d2df18ff1
busra <b.busraayilmaz@gmail.com>
2024-02-05 21:38:30 +0300

    formik-added-to-GeneralForm&PortalFormCreated

---
+++ form-project/src/Components/GeneralForm.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Formik, useFormik } from "formik";
+
+function GeneralForm() {
+  const { values, errors, handleChange, handleSubmit } = useFormik({
+    initialValues: {
+      email: "",
+      age: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  return (
+    <form>
+      <div className="inputDiv">
+        <label>Mail Adresinizi Girin</label>
+        <input
+          type="mail"
+          value={values.email}
+          onChange={handleChange}
+          id="email"
+          placeholder="Mailinizi Girin"
+        />
+      </div>
+
+      <div className="inputDiv">
+        <label>Yaşınızı Girin</label>
+        <input
+          type="number"
+          value={values.age}
+          onChange={handleChange}
+          id="age"
+          placeholder="Yaşınızı Girin"
+        />
+      </div>
+
+      <div className="inputDiv">
+        <label>Şifre</label>
+        <input
+          type="password"
+          value={values.password}
+          onChange={handleChange}
+          id="password"
+          placeholder="Şifrenizi Girin"
+        />
+      </div>
+
+      <div className="inputDiv">
+        <label>Şifre Tekrar</label>
+
+        <input
+          type="password"
+          value={values.confirmPassword}
+          onChange={handleChange}
+          id="confirmPassword"
+          placeholder="Şifreyi Tekrar Girin"
+        />
+      </div>
+    </form>
+  );
+}
+
+export default GeneralForm;

37069262bb4d8b452fd1abefc247226529430772
busra <b.busraayilmaz@gmail.com>
2024-02-10 00:43:39 +0300

    router-deleted

--- form-project/src/Components/PortalForm.js
+++ form-project/src/Components/PortalForm.js
@@ -1,8 +1,9 @@
 import React from "react";
-import {} from "react-router-dom";
+
 import CustomInput from "./CustomInput";
 import { Form, Formik } from "formik";
 import { advancedSchema } from "../schemass";
+import CustomSelect from "./CustomSelect";
 
 function PortalForm() {
   const onSubmit = async (values, actions) => {
@@ -33,6 +34,18 @@ function PortalForm() {
               type="text"
               placeholder="Kullanici Adinizi Girin"
             />
+            <CustomSelect
+              label="Okulunuz"
+              name="university"
+              placeholder="Kullanici Okuduğunuz Üniversiteyi Seçiniz"
+            >
+              <option value="">Lütfen Üniversitenizi Seçin</option>
+              <option value="bogazici">Boğaziçi Üniversitesi</option>
+              <option value="gsu">Galatasaray Üniversitesi</option>
+              <option value="itü">İstanbul Teknik Üniversitesi</option>
+              <option value="ytü">Yıldız Teknik Üniversitesi</option>
+              <option value="odtü">ODTÜ</option>
+            </CustomSelect>
           </Form>
         )}
       </Formik>

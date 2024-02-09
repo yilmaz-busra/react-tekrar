98617aae3072a218a375211333c0778ca16fdf68
busra <b.busraayilmaz@gmail.com>
2024-02-09 23:57:06 +0300

    FomikAdded

--- form-project/src/Components/PortalForm.js
+++ form-project/src/Components/PortalForm.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { Form, Formik, Field } from "react-router-dom";
 
 function PortalForm() {
-  return <div>PortalForm</div>;
+  const onSubmit = async (values, actions) => {
+    console.log(values);
+    console.log(actions);
+
+    await new Promise((resolve) => {
+      setTimeout(resolve, 1000);
+    });
+    actions.resetForm();
+  };
+  return (
+    <>
+      <Formik
+        initialValues={{
+          userName: "",
+          university: "red",
+          isAccepted: false,
+        }}
+        onSubmit={onSubmit}
+      >
+        {() => <Form></Form>}
+      </Formik>
+    </>
+  );
 }
 
 export default PortalForm;
